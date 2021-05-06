106e36e3c4397a0bec31c610bc797d41403372ff
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-06 19:22:32 +0530

    sm6250-common: Panic to recovery, not bootloader

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,6 +101,7 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
