674c4506925fd1bec1f681392a26926e9cd82c4f
Alexander Koskovich <zvnexus@outlook.com>
2021-08-06 22:29:21 +0530

    sm6250-common: Remove virtual framebuffer.  * It's largely for testing purposes and it consumes    a large amount of memory. If your hardware doesn't    have graphics then it can be useful, which isn't the    case for shipped devices.
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9
    Signed-off-by: adiatul16 <adiatul16@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,7 +94,6 @@ BOARD_KERNEL_CMDLINE += msm_rtb.enabled=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
