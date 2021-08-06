c4cac0b5f79a76c6d351e8e365089c0f91439b9b
adiatul16 <adiatul16@gmail.com>
2021-08-06 22:39:18 +0530

    sm6250-common: Build AIDL power stats * Fix for aidl power stats errors. - Now with this we fixing both errors: BatteryStatsService: Unable to load Power Hal or power.stats HAL hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest. hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest.
    Signed-off-by: adiatul16 <adiatul16@gmail.com>

--- common.mk
+++ common.mk
@@ -308,7 +308,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.2.vendor \
+    android.hardware.power.stats@1.0-service.mock
 
 # Recovery
 PRODUCT_COPY_FILES += \
