45d6088a65bc81fc77cbecae3aa8b55182d7b940
dereference23 <dereference23@outlook.com>
2022-07-22 06:29:01 -0400

    sm6250-common: Add framework compatibility matrix
    Signed-off-by: Mohammad Nobakht <mohammadnt96@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,7 +102,9 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += kpti=off
+
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
