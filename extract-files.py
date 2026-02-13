41966f886ffd21e35ba444ab7ea111effc05a693
New Author Name <arian.kulmer@web.de>
2026-02-13 10:16:26 +0530

    sm6250-common: Bring back face detection lib
    Change-Id: I50af944e9398eef352f72f4729999f8d0c556837
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- extract-files.py
+++ extract-files.py
@@ -86,6 +86,10 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libvendor.goodix.hardware.biometrics.fingerprint@2.1.so', 'vendor.goodix.hardware.biometrics.fingerprint@2.1.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    ('vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/libFaceDetectpp-0.5.2.so', 'vendor/lib64/libfacedet.so'): blob_fixup()
+        .binary_regex_replace(b'libmegface.so', b'libfacedet.so')
+        .binary_regex_replace(b'libMegviiFacepp-0.5.2.so', b'libFaceDetectpp-0.5.2.so')
+        .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
