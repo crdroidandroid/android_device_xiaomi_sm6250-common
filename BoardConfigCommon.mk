361a3c11663257999e21cb02eee9530bfec16f33
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-04-12 14:24:52 +0530

    sm6250-common: Import c2_manifest_vendor.xml to common
    - For Dolby Vision
    
    Change-Id: I59ceeec26346c0a04a636fa840218dd75b703b7b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,6 +56,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/hidl/c2_manifest_vendor.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
