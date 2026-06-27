8d9b1334ec2110b258ea5930e86de4a1974e9e49
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-06-27 22:22:08 +0530

    sm6250-common: Add custom volumes Support as an optional
    - https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/4aef9d6fc52a5e0692c0fd2eaa0c8463e95ae729
    - https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/ce4afe463ab33a2edac3b73d6f06f27df31372e3
    
    * TARGET_INCLUDES_CUSTOM_VOLUMES := true
    
    Change-Id: I1b928ba6c7e7d3a48ba9d1beecd9a00ce22ee60f

--- atoll.mk
+++ atoll.mk
@@ -51,10 +51,19 @@ PRODUCT_COPY_FILES += \
      $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+
+ifeq ($(TARGET_INCLUDES_CUSTOM_VOLUMES),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+else
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+endif
 
 # Camera
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
